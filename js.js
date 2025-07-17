2a2d40d60d61f3b983ed17e2920ba8b8247c4bbc
Ahmad-byte-design <www.ahmadhusensy@gmail.com>
2025-07-17 13:31:18 +0300

    add hangman by ahmad al hoseen made with html and css

---
+++ js.js
@@ -0,0 +1,15 @@
+// letters
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+// get array from letters
+
+let lettersarr = Array.from(letters);
+
+// select letters container
+
+let lettersContainer = document.querySelector(".letters")
+
+// generate letters
+lettersarr.forEach( letter => {
+    
+})
